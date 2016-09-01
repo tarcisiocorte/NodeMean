76347923649f669122ab30ad178ae87fd92c81f5
Tarcisio Costa Corte de Paula <tarcisiocorte@gmail.com>
2016-09-02 00:12:36 +0100

    the first directive has been added

--- SubApps/alurapic/public/js/controllers/fotos-controller.js
+++ SubApps/alurapic/public/js/controllers/fotos-controller.js
@@ -1,8 +1,14 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
 	
-	$scope.foto = {
-		titulo : 'Leão',
-		url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-	}; 
+	$scope.fotos = []; 
+
+	$http.get('/v1/fotos')
+	.success(function(retorno) {
+		console.log(retorno);
+		$scope.fotos = retorno; // não precisa fazer retorno.data
+	})
+	.error(function(erro) {
+		console.log(erro);
+	});
 
 });
