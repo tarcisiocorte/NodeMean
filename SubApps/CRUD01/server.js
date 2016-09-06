3fd00642c422657b0e49dd1b6ac4e92cdfdd32e3
SPFarm <SPFarm@dev.pobaldev>
2016-09-06 11:53:35 +0000

    The body-parser first POST

--- SubApps/CRUD01/server.js
+++ SubApps/CRUD01/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({executed: true}))
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/quotes',function(req, res){
-  console.log('The POST has been executed.')
+  console.log(req.body);
 })
 
 app.listen(3000, function() {
