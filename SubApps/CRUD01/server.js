1f52a58d64e605d4482f4252e3c13d0c69615448
SPFarm <SPFarm@dev.pobaldev>
2016-09-06 17:25:49 +0000

    POST method - sub project CRUD

--- SubApps/CRUD01/server.js
+++ SubApps/CRUD01/server.js
@@ -8,10 +8,28 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.post('/quotes',function(req, res){
-  console.log(req.body);
+var db
+const MongoClient = require('mongodb').MongoClient
+MongoClient.connect('mongodb://tarcisio:1234.abcd@ds019946.mlab.com:19946/tarcisio-mongodb-crud', function(err, database){
+  if (err) return console.log(err)
+  db = database
+  console.log('connected...')
+    app.listen(3000, function() {
+      console.log('listening on 3000')
+    })
 })
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
+app.post('/quotes',function(req, res){
+  db.collection('quotes').save(req.body,function(err, result){
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.get('/',function(req, res){
+  var cursor = db.collection('quotes').find()
+  .toArray(function(err, results){
+    console.log(results)
+  })
 })
