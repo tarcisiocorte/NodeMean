67ffdbe7c0014f1f6b8ca41ba5baad9975f0cc01
SPFarm <SPFarm@dev.pobaldev>
2016-09-06 17:43:25 +0000

    The complete CRUD

--- SubApps/CRUD01/server.js
+++ SubApps/CRUD01/server.js
@@ -1,35 +1,57 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-
-app.use(bodyParser.urlencoded({executed: true}))
-
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html')
-})
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 
 var db
-const MongoClient = require('mongodb').MongoClient
-MongoClient.connect('mongodb://tarcisio:1234.abcd@ds019946.mlab.com:19946/tarcisio-mongodb-crud', function(err, database){
+
+MongoClient.connect('mongodb://zellwk:zellwk@ds047955.mongolab.com:47955/star-wars-quotes', (err, database) => {
   if (err) return console.log(err)
   db = database
-  console.log('connected...')
-    app.listen(3000, function() {
-      console.log('listening on 3000')
-    })
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('listening on 3000')
+  })
 })
 
-app.post('/quotes',function(req, res){
-  db.collection('quotes').save(req.body,function(err, result){
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {quotes: result})
+  })
+})
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
     res.redirect('/')
   })
 })
 
-app.get('/',function(req, res){
-  var cursor = db.collection('quotes').find()
-  .toArray(function(err, results){
-    console.log(results)
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({name: 'Yoda'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/quotes', (req, res) => {
+  db.collection('quotes').findOneAndDelete({name: req.body.name}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('A darth vadar quote got deleted')
   })
 })
