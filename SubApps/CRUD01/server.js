436ac78048bca2d25645213754eda524ef55e3c3
SPFarm <SPFarm@dev.pobaldev>
2016-09-06 11:47:26 +0000

    The first POST has been added.

--- SubApps/CRUD01/server.js
+++ SubApps/CRUD01/server.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
+app.post('/quotes',function(req, res){
+  console.log('The POST has been executed.')
+})
+
 app.listen(3000, function() {
   console.log('listening on 3000')
 })
