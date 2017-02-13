f95fe344c4565817240a659a86efc25eff8e032c
tarci <tarcisiocorte@gmail.com>
2017-02-13 20:55:27 -0200

    remove prod connection

--- Server/config/config.js
+++ Server/config/config.js
@@ -1,3 +1,10 @@
+var connectionToProduction = '';
+if(process.env.NODE_ENV == 'production'){
+  console.log('production is running...');
+  connectionToProduction = require('./prodCon')();
+  console.log('This is the connection: ' + connectionToProduction);
+}
+
 var path = require('path');
 var rootPath = path.normalize(__dirname + '/../../');
 
@@ -9,7 +16,7 @@ module.exports = {
   },
   production: {
     rootPath: rootPath,
-    db: 'mongodb://tarcisio:1234@ds139619.mlab.com:39619/multivision',
-    port: process.env.PORT || 80
+    db: connectionToProduction,
+    port: process.env.PORT || 3030
   }
 }
