0e64abe46c938914c747b0e6cc6edecda1aab139
tarcisiocorte <tarcisiocorte@gmail.com>
2017-09-06 11:45:19 -0300

    routes refactoring

--- Server/config/routes.js
+++ Server/config/routes.js
@@ -1,9 +1,3 @@
-var auth = require('./auth'),
-  courses = require('../controllers/courses'),
-  mongoose = require('mongoose'),
-  User = mongoose.model('User'),
-  userRoute = require('../routes/users');
-
 module.exports = function(app) {
 
   require('../routes/courses')(app);
@@ -12,12 +6,5 @@ module.exports = function(app) {
   app.all('/api/*', function(req, res) {
     res.send(404);
   });
-
-  app.get('*', function(req, res) {
-    res.render('index', {
-      bootstrappedUser: req.user
-    });
-  });
-  
 }
 
